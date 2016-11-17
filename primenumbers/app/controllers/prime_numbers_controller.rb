c8b067541ffbe3556bcb8be9d03976f807d17456
bluefalcon26 <daprejean@gmail.com>
2016-11-17 13:35:23 -0600

    Add tests for PrimeNumberController, and implement.

--- primenumbers/app/controllers/prime_numbers_controller.rb
+++ primenumbers/app/controllers/prime_numbers_controller.rb
@@ -1,4 +1,13 @@
+require 'prime'
+require 'json'
+
 class PrimeNumbersController < ApplicationController
-  def get
+  def index
+  end
+
+  def show
+    @results = Prime.take(1000)
+
+    render json: @results.to_json
   end
 end
--- primenumbers/config/routes.rb
+++ primenumbers/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'prime_numbers/get'
+  get 'prime_numbers/index'
+  get 'prime_numbers/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
